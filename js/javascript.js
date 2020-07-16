de2aa488fca2b62e33e67f9a541dc851172fedda
lloh118 <55875770+lloh118@users.noreply.github.com>
2020-07-16 12:31:04 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,83 @@
 
+//DATA LOCATION PAGE
+var loc = document.getElementById("location");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        loc.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+//show the position function
+function showPosition(position) {
+    console.log("show position() called");
+    alert(" Your latitude is " + position.coords.latitude);
+    //jQuery for Display value to html doc by updated loc variable value
+    $(loc).val("<p>Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude + "</p>");
+
+}
+
+
+//Global variables
+
+var watchID;
+
+var locationOptions = {
+    maximumAge: 10000,
+    timeout: 6000,
+    enableHighAccuracy: true
+};
+
+//when the jQuery Mobile page is initialised
+$(document).on('pageinit', function() {
+
+    console.log("jQuery mobile intialised");
+    //set up listener for button clicks
+    $('#startLocationButton').on('click', updatePosition);
+    $('#stopLocationButton').on('click', stopPosition);
+
+});
+
+
+
+//fucntion to Update changes in the position via button in html
+function updatePosition() {
+    //instruct location service to get position with appropriate callbacks
+    watchID = navigator.geolocation.watchPosition(successPosition, failPosition, locationOptions);
+}
+
+//Stop watching for updates in location
+function stopPosition() {
+    //instruct location service to get position with appropriate callbacks
+    navigator.geolocation.clearWatch(watchID);
+}
+
+
+//called when the position is successfully determined
+function successPosition(position) {
+
+    //save the below long/latitute/time in varaibles below ready to export to html
+    var time = position.timestamp;
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    //jQuery for inputting the values into the html document
+    $('#time').val("Recieved data at " + time);
+    $('#lattext').val(latitude);
+    $('#longtext').val(longitude);
+
+}
+
+//called if the position throws back error not success
+function failPosition(error) {
+    //change time box to show updated message
+    $('#time').val("Sorry we can't get your data right now: " + error);
+
+}
+
+//MAP PAGE SECTION
 
 
 function onMapInit(map) {
@@ -30,7 +109,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoiMTgwMDkwMzciLCJhIjoiY2tjbnd2MW54MGZnNzJzcGIyYWt4eTI4bSJ9.7bmgBrSFIPDj3xeSYOkkEQ'
 }).addTo(mymap);
 
-
+//Emergency call button would be linked to server but for now just alert
 function emerCall() {
     alert("Emergency call sent to server")
 
@@ -38,18 +117,11 @@ function emerCall() {
 $( document ).ready(function() {
 
 });
-var loc = document.getElementById("location");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        loc.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
 
-function showPosition(position) {
-    console.log("show position() called");
-    alert(" Your latitude is " + position.coords.latitude);
-    loc.innerHTML = "<p>Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude + "</p>";
-}
+
+
+
+
+
+
+
